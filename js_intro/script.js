3a0b13c944ab5efcb5aac17dc8c32ae64a703b10
Sachin7717 <sachindhimann71@gmail.com>
2024-02-24 20:37:22 +0530

    learned about typeof

--- js_intro/script.js
+++ js_intro/script.js
@@ -2,6 +2,7 @@ console.log("hii this is my first js code");
 
 let a =5;
 let b =8;
+let c= "sachin";
 
 console.log(a+b+5);
 
@@ -9,3 +10,4 @@ console.log(a+b+5);
     let a=34;
     console.log(a);
 }
+console.log(typeof a, typeof b,typeof c)
