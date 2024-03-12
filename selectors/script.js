1b01087063041214464f5ec3b5723d429dea205c
Sachin7717 <sachindhimann71@gmail.com>
2024-03-12 10:27:34 +0530

    selectors in js

---
+++ selectors/script.js
@@ -0,0 +1,7 @@
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+boxes[2].style.backgroundColor = "yellow";
+console.log("hii");
+
+document.getElementById("redbox").style.backgroundColor="red";
+document.querySelector(".box").style.backgroundColor="purple";
