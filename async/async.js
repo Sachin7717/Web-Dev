80d78654aa25250ad85bc9cea0095787d739f4d2
Sachin7717 <sachindhimann71@gmail.com>
2024-03-15 20:43:04 +0530

    post request in api

--- async/async.js
+++ async/async.js
@@ -8,13 +8,23 @@
 // }
 async function getdata() {
     // Simulate getting data from a server
-    let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-    // let x = await fetch('https://jsonplaceholder.typicode.com/posts')
- 
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
     let data = await x.json() 
     return data
 }
+
 
 
 async function main(params) {
@@ -27,7 +37,6 @@ async function main(params) {
 }
 
 main();
-
 
   
 
